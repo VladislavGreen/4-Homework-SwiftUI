064f764c5a86d2502bce0f793bb1c27a60817c86
Vladislav Green <vladzel@gmail.com>
2023-02-27 10:15:38 +0300

    Initial Commit

---
+++ 4 Homework SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  4 Homework SwiftUI
+//
+//  Created by Vladislav Green on 2/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 4 Homework SwiftUI/__Homework_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  __Homework_SwiftUIApp.swift
+//  4 Homework SwiftUI
+//
+//  Created by Vladislav Green on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct __Homework_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
